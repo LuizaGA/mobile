9998c1ac1b65d7f204f55ca9a8309588e2ea2fda
Samuel Façanha <samuelfacanha@edu.unifor.br>
2021-11-29 11:16:16 -0300

    tudo funcionando

--- App.js
+++ App.js
@@ -1,15 +1,67 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Alert,
+  Pressable,
+} from "react-native";
+import api from "./api";
 
 export default function App() {
+  const [brand, setBrand] = useState("");
+  const [quantity, setQuantity] = useState("");
+  const [price, setPrice] = useState("");
+
+  async function AdicionarCompras() {
+    const dados = {
+      brand,
+      quantity,
+      price,
+    };
+    const response = await api.post("register", dados).then((response) => {
+      Alert.alert("", "Compra Adicionada com Sucesso", [
+        { text: "OK", onPress: () => console.log("OK Pressed") },
+      ]);
+      setBrand("");
+      setQuantity("");
+      setPrice("");
+    });
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Text style={styles.titulo}>Adicionar Compra</Text>
       <Text style={styles.texto}>Marca: </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Marca X"
+        onChangeText={(brand) => setBrand(brand)}
+        defaultValue={brand}
+        keyboardType="default"
+      ></TextInput>
       <Text style={styles.texto}>Quantidade: </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="20"
+        onChangeText={(quantity) => setQuantity(quantity)}
+        defaultValue={quantity}
+        keyboardType="numeric"
+      ></TextInput>
       <Text style={styles.texto}>Preço: </Text>
+      <TextInput
+        style={styles.input}
+        placeholder="100"
+        onChangeText={(price) => setPrice(price)}
+        defaultValue={price}
+        keyboardType="numeric"
+      ></TextInput>
+      <Pressable style={styles.botao} onPress={() => AdicionarCompras()}>
+        <Text style={styles.botaoTexto}>Adicionar Compras</Text>
+      </Pressable>
     </View>
   );
 }
@@ -24,9 +76,34 @@ const styles = StyleSheet.create({
 
   titulo: {
     fontSize: 30,
+    fontWeight: "bold",
+    color: "darkgreen",
+    marginBottom: 20,
   },
 
   texto: {
     fontSize: 20,
+    color: "green",
+  },
+
+  input: {
+    height: 40,
+    width: 300,
+    borderWidth: 1,
+    margin: 10,
+  },
+
+  botao: {
+    marginTop: 10,
+    backgroundColor: "darkgreen",
+    height: 40,
+    width: 300,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  botaoTexto: {
+    fontSize: 20,
+    color: "white",
   },
 });
