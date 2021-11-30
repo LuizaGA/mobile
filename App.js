d4e15aa482556ddbf37f90d5b6f6ffa7113bc31c
Samuel Façanha <samuelfacanha@edu.unifor.br>
2021-11-30 11:27:30 -0300

    final

--- App.js
+++ App.js
@@ -33,6 +33,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.header}>Desafio Incrivel</Text>
+      <Text style={styles.zeus}>Zeus</Text>
       <StatusBar style="auto" />
       <Text style={styles.titulo}>Adicionar Compra</Text>
       <Text style={styles.texto}>Marca: </Text>
@@ -42,6 +44,7 @@ export default function App() {
         onChangeText={(brand) => setBrand(brand)}
         defaultValue={brand}
         keyboardType="default"
+        maxLength={20}
       ></TextInput>
       <Text style={styles.texto}>Quantidade: </Text>
       <TextInput
@@ -50,6 +53,7 @@ export default function App() {
         onChangeText={(quantity) => setQuantity(quantity)}
         defaultValue={quantity}
         keyboardType="numeric"
+        maxLength={4}
       ></TextInput>
       <Text style={styles.texto}>Preço: </Text>
       <TextInput
@@ -58,6 +62,7 @@ export default function App() {
         onChangeText={(price) => setPrice(price)}
         defaultValue={price}
         keyboardType="numeric"
+        maxLength={6}
       ></TextInput>
       <Pressable style={styles.botao} onPress={() => AdicionarCompras()}>
         <Text style={styles.botaoTexto}>Adicionar Compras</Text>
@@ -69,41 +74,61 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#2b2b6f",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: "bold",
+    color: "#fff",
+    margin: 5,
+    alignSelf: "center",
+  },
+  zeus: {
+    fontSize: 45,
+    fontWeight: "bold",
+    color: "#fff",
+    margin: 15,
+    alignSelf: "center",
   },
 
   titulo: {
-    fontSize: 30,
+    fontSize: 25,
     fontWeight: "bold",
-    color: "darkgreen",
+    color: "#fff",
+    marginTop: 10,
     marginBottom: 20,
   },
 
   texto: {
     fontSize: 20,
-    color: "green",
+    color: "#fff",
   },
 
   input: {
     height: 40,
     width: 300,
     borderWidth: 1,
+    borderColor: "#fff",
     margin: 10,
+    color: "white",
+    borderRadius: 10,
   },
 
   botao: {
-    marginTop: 10,
-    backgroundColor: "darkgreen",
+    marginTop: 15,
+    backgroundColor: "#fff",
     height: 40,
     width: 300,
     alignItems: "center",
     justifyContent: "center",
+    borderRadius: 10,
   },
 
   botaoTexto: {
     fontSize: 20,
-    color: "white",
+    color: "#2b2b6f",
+    fontWeight: "bold",
   },
 });
